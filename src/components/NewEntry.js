7c519164ccc3499f19df59602581c3be902321f0
meghakak <meghakak1@gmail.com>
2020-09-19 15:25:44 -0700

    Add post request for new entry.

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from './NavBar.js';
 import './ViewEntry.css';
 import './NewEntry.css';
+import { findAllInRenderedTree } from 'react-dom/test-utils';
+import axios from 'axios';
 
 const NewEntry = () => {
-    const currentDate = (new Date()).toDateString();
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post(`https://localhost5000/entry`, { title, Date: new Date(), body })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    };
+    
     return (
         <div>
             <NavBar title = 'new journal entry' />
             <div id="entry">
-                <form id="new-entry-form" className="center">
-                    <input type="text" id="entry-title" className="center spacing" placeholder="Enter a title here..." required></input>
+                <form id="new-entry-form" className="center" onSubmit={handleSubmit}>
+                    <input type="text" id="entry-title" className="center spacing" onChange={(e) => {setTitle(e.target.value)}} value={title} placeholder="Enter a title here..." required></input>
                     <br></br>
-                    <textarea rows="5" id="entry-snippet" className="spacing" form="new-entry-form" placeholder="Begin your journal entry here..." required></textarea>
+                    <textarea rows="5" id="entry-snippet" className="spacing" onChange={(e) => {setBody(e.target.value)}} value={body} form="new-entry-form" placeholder="Begin your journal entry here..." required></textarea>
                     <br></br>
                     <button className="button center done">Done</button>
                 </form> 
