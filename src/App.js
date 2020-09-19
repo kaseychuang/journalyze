8c3f5c3d620fa3780b8af5d8634aad9b696bf4d9
Kasey Chuang <chuangky@uci.edu>
2020-09-19 00:53:11 -0700

    finished entry component ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home.js';
+import ViewEntries from './components/ViewEntries.js';
+import ViewEntry from './components/ViewEntry.js';
 
 class App extends React.Component{
     render(){
         return(
             <Router>
                 <div>
-                    <Route path = "/" exact render = {Home} />
+                    <Route path = "/" exact component = {Home} />
+                    <Route path = "/entries" exact component = {ViewEntries} />
+                    <Route path = "/entry" exact component = {ViewEntry} />
+
                 </div>
             </Router>
         )
--- src/components/ViewEntries.js
+++ src/components/ViewEntries.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Entry from './Entry';
 
+// clicking on an entry will send you to the ViewEntry for that speciic page
+
 const ViewEntries = () => {
     return (
         <div>
-            ViewEntries
-            <Entry />
-            <Entry /> 
+            <h1>View Entries</h1>
+            <div id = "options">
+                <a className = "button" href = "#">new journal entry</a>
+                <a className = "button" href = "#">view stats</a>
+            </div>
+            <Entry key = "id 123" title = "Bad Day" date = "6/14/20" snippet = "Hello therekajskldfjasldkjf" />
+            <Entry key = "id here" title = "Terrible Day" date = "6/15/20" snippet = "Sed ut persptur?"/> 
         </div>
     )
 }
