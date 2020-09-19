6023400e7da90e2b30e493cf19138f45e51125f1
Gayatri Subramaniam <gayatrs@uci.edu>
2020-09-18 21:54:23 -0700

    Added python file with flask

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, send_file, render_template
+import json
+
+app = Flask(__name__, static_url_path='', static_folder="public")
+
+@app.route('/')
+def demo():
+    return json.dump("hi")
+
+app.run(host="127.0.0.1", port="80")
