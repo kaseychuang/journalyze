9b61e4ff6b5b95d735601fdf6324e0a51e5ba74b
Kasey Chuang <chuangky@uci.edu>
2020-09-19 17:31:23 -0700

    get specific entry data and display

--- routes/EntryRoute.js
+++ routes/EntryRoute.js
@@ -3,8 +3,21 @@ const router = express.Router();
 
 const PostEntry = require('../models/PostEntry.js')
 
-router.get('/entry', (req, res) => {
+router.get('/entry/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const entry = await PostEntry.findById(id);
+        if (!entry){
+            return res.status(400).send({error: "Couldn't find that entry"});
+        }
+        res.status(200).send(entry);
+    }catch (e){
+        res.status(500).send(e)
+    }
+    
+})
 
+router.get('/entries', (req, res) => {
     PostEntry.find({})
     .then((data) => {
         console.log('Data:');
@@ -13,8 +26,6 @@ router.get('/entry', (req, res) => {
     .catch((error) => {
         console.log('Error: ', error);
     });
-
-    
 });
 
 router.post('/save', (req, res) => {
