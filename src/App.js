695af7750cfc6b29d7a75a2a32c3e80290b5eef0
Danielle Muhlenberg <danieyells@gmail.com>
2020-10-04 17:54:17 -0700

    Changed navbar to a class to be able to add conditional styling on active links, home and moodivities are underlined whenever active

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home.js';
 import Moodivities from './components/Moodivities.js'
 import './Util.css';
@@ -7,17 +8,18 @@ import NewEntry from './components/NewEntry.js'
 import ViewEntry from './components/ViewEntry.js';
 import ViewStats from './components/ViewStats.js';
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+ 
+    render() {
+        return (
             <Router>
                 <div>
                     <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'></link>
-                    <Route path = '/' exact component = {Home} />
-                    <Route path = '/moodivities' exact component = {Moodivities} />
-                    <Route path = '/entry/:id' exact component = {ViewEntry} />
-                    <Route path = '/new-entry' exact component = {NewEntry} />
-                    <Route path = "/stats" exact component = {ViewStats} />
+                    <Route path='/' exact component={Home} />
+                    <Route path='/moodivities' exact component={Moodivities} />
+                    <Route path='/entry/:id' exact component={ViewEntry} />
+                    <Route path='/new-entry' exact component={NewEntry} />
+                    <Route path="/stats" exact component={ViewStats} />
                 </div>
             </Router>
         )
